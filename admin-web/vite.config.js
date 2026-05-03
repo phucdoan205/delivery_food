d35906cc8a413fbf61686cd187d91d2e7b45dc3b
Đoàn Văn Phúc <phucvandoan123@gmail.com>
2026-05-03 22:06:53 +0700

    update code toàn bộ

--- admin-web/vite.config.js
+++ admin-web/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // Đây là chìa khóa để hết lỗi
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tailwindcss(), // Phải đặt cái này lên trên
+    react(),
+  ],
+});
